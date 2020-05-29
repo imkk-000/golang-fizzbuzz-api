d1ab30225f9e416e99f85676ea3fc2410ed358ea
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-05-29 22:21:29 +0700

    Update ATDD setModulationSequence

--- atdd/atdd_test.go
+++ atdd/atdd_test.go
@@ -1,6 +1,7 @@
 package atdd_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -8,15 +9,50 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"fizzbuzz-api/model"
 )
 
 const HOST = "http://localhost:8080"
 
-func TestATDDSendToUpdateModulationSequence(t *testing.T) {
+func TestATDDSendToSetModulationSequence(t *testing.T) {
+	assert := assert.New(t)
+	modulationSequence := &model.ModulationSequenceRequest{
+		ModulationSequences: []model.ModulationSequence{
+			{
+				Key:   3,
+				Value: "Fizz",
+			},
+			{
+				Key:   5,
+				Value: "Buzz",
+			},
+		},
+	}
+	url := fmt.Sprintf("%s/setModulationSequence", HOST)
+	reqBody, err := json.Marshal(modulationSequence)
+	assert.NoError(err)
+	assert.NotEmpty(reqBody)
 
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(reqBody))
+	assert.NoError(err)
+	assert.NotNil(resp)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(err)
+	assert.NotEmpty(respBody)
+
+	actualResp := &model.ModulationSequenceResponse{}
+	err = json.Unmarshal(respBody, actualResp)
+	assert.NoError(err)
+
+	assert.NotZero(actualResp.UpdateCount)
+	assert.NotEmpty(actualResp.ModulationSequences)
+	assert.Equal(modulationSequence.ModulationSequences, actualResp.ModulationSequences)
 }
 
-func TestATDDSayNumber(t *testing.T) {
+func TestATDDSendNumberToSayWithSuccess(t *testing.T) {
 	assert := assert.New(t)
 	sayNumbers := map[int]string{
 		1:  "1",
@@ -38,11 +74,8 @@ func TestATDDSayNumber(t *testing.T) {
 		assert.NoError(err)
 		assert.NotEmpty(respBody)
 
-		actualSayNumber := struct {
-			SaidCount int
-			Say       string
-		}{}
-		err = json.Unmarshal(respBody, &actualSayNumber)
+		actualSayNumber := &model.SayingNumberResponse{}
+		err = json.Unmarshal(respBody, actualSayNumber)
 		assert.NoError(err)
 
 		assert.NotZero(actualSayNumber.SaidCount)
